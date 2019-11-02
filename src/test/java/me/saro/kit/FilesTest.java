9d67baebf25f7993be7626d0c3c1a8182fdd5f67
PARK Yong Seo <j@saro.me>
2019-11-02 23:20:27 +0900

    test set

---
+++ src/test/java/me/saro/kit/FilesTest.java
@@ -0,0 +1,15 @@
+package me.saro.kit;
+
+import org.junit.jupiter.api.Test;
+
+public class FilesTest {
+
+    @Test
+    public void testLambdas() throws Exception {
+
+
+
+    }
+
+    
+}
---
+++ src/test/java/me/saro/kit/IOStreamsTest.java
@@ -0,0 +1,15 @@
+package me.saro.kit;
+
+import org.junit.jupiter.api.Test;
+
+public class IOStreamsTest {
+
+    @Test
+    public void testLambdas() throws Exception {
+
+
+
+    }
+
+    
+}
