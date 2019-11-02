1738e596a30f3ff437d9311cb2faf495dbfc205b
PARK Yong Seo <j@saro.me>
2019-11-02 23:20:27 +0900

    test set

---
+++ src/test/java/me/saro/kit/DatesTest.java
@@ -0,0 +1,21 @@
+package me.saro.kit;
+
+import me.saro.kit.function.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DatesTest {
+
+    @Test
+    public void testLambdas() throws Exception {
+
+
+
+    }
+
+
+}
