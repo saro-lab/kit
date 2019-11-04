bc65209e25f5c8e50f96f5f6ae6d5c3cd39af539
PARK Yong Seo <j@saro.me>
2019-11-04 18:49:48 +0900

    test

--- src/main/java/me/saro/kit/ClassSerializer.java
+++ src/main/java/me/saro/kit/ClassSerializer.java
@@ -1,34 +1,56 @@
 package me.saro.kit;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  *
  * @param <T>
  */
 public abstract class ClassSerializer<T> {
 
-    public T deserialize(String data) {
-        deserialize(new ByteData(data, charset()));
+    public T read(String data) {
+        read(new ByteData(data, charsetByteData()));
         return (T)this;
     }
 
-    public T deserialize(byte[] data) {
-        deserialize(new ByteData(data, charset()));
+    public T read(byte[] data) {
+        read(new ByteData(data, charsetByteData()));
         return (T)this;
     }
 
-    public byte[] serialize() {
-        ByteData data = new ByteData(limit(), charset());
-        serialize(data);
+    public byte[] toBytes() {
+        ByteData data = new ByteData(limitSizeByteData(), charsetByteData());
+        write(data);
         return data.toBytes();
     }
 
-    protected String charset() {
+    public String toSerializeString() {
+        try {
+            return new String(toBytes(), charsetByteData());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("this system does not support `"+charsetByteData()+"` encoding");
+        }
+    }
+
+    protected String charsetByteData() {
         return "UTF-8";
     }
 
-    protected abstract void deserialize(ByteData data);
+    /**
+     * read byteData
+     * @param data
+     */
+    protected abstract void read(ByteData data);
 
-    protected abstract void serialize(ByteData data);
+    /**
+     * write byteData
+     * @param data
+     */
+    protected abstract void write(ByteData data);
 
-    protected abstract int limit();
+    /**
+     * limit size byte data
+     * @return
+     */
+    protected abstract int limitSizeByteData();
 }
