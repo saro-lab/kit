9f01abbfdd7654bd40d59a63edab6661c090f428
PARK Yong Seo <j@saro.me>
2019-11-11 23:54:07 +0900

    remove UnsupportedEncodingException 2

--- src/test/java/me/saro/kit/SimpleCryptTest.java
+++ src/test/java/me/saro/kit/SimpleCryptTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -20,20 +20,20 @@ public class SimpleCryptTest {
     final static byte[] iv = "12345qwert^%@!@f".getBytes();
     
     @Test
-    public void cryptText() throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+    public void cryptText() throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
         
         SimpleCrypt en = SimpleCrypt.encrypt("AES/CBC/PKCS5Padding", key, iv);
         SimpleCrypt de = SimpleCrypt.decrypt("AES/CBC/PKCS5Padding", key, iv);
         
         String text = "data";
-        String encrypt = en.toHex(text.getBytes(CHARSET));
-        String decrypt = new String(de.toBytesByHex(encrypt), CHARSET);
+        String encrypt = en.toHex(text.getBytes(Charset.forName(CHARSET)));
+        String decrypt = new String(de.toBytesByHex(encrypt), Charset.forName(CHARSET));
         
         assertEquals(text, decrypt);
         
         text = "data";
-        encrypt = en.toBase64(text.getBytes(CHARSET));
-        decrypt = new String(de.toBytesByBase64(encrypt), CHARSET);
+        encrypt = en.toBase64(text.getBytes(Charset.forName(CHARSET)));
+        decrypt = new String(de.toBytesByBase64(encrypt), Charset.forName(CHARSET));
         
         assertEquals(text, decrypt);
     }
