18b186d263b18cc589d1096feec3dbc23849fa88
PARK Yong Seo <j@saro.me>
2020-03-11 04:34:57 +0900

    test

---
+++ src/test/java/me/saro/kit/WebTest.java
@@ -0,0 +1,23 @@
+package me.saro.kit;
+
+import me.saro.kit.webs.Web;
+
+class WebTest {
+
+    //@Test
+    public void test() {
+
+        Web.applyGlobalIgnoreCertificate();
+
+        System.out.println("value 1");
+        System.out.println(Web.get("https://saro.me").result());
+
+        System.out.println("value 2");
+        System.out.println(Web.get("https://111.saro11.me").result());
+        System.out.println(Web.get("https://111.saro11.me").result().getBody("error text"));
+
+        System.out.println("value 3");
+        System.out.println(Web.get("http://localhost:8080/api/board/any/recent").result());
+        System.out.println(Web.get("http://localhost:8080/api/board/any/recent").result().getBody("error text"));
+    }
+}
