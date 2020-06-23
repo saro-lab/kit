96724ad71225ac6a0fc4e710a28022e5eb6deda1
PARK Yong Seo <j@saro.me>
2020-06-23 15:53:19 +0900

    add merge functions

--- src/main/java/me/saro/kit/Lists.java
+++ src/main/java/me/saro/kit/Lists.java
@@ -1,6 +1,8 @@
 package me.saro.kit;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -15,7 +17,6 @@ public class Lists {
      * @param <T> template
      * @return editable list (ArrayList<T>)
      */
-    @SafeVarargs
     public static <T> List<T> asList(T... args) {
         List<T> list = new ArrayList<>();
         if (args != null) {
@@ -24,5 +25,41 @@ public class Lists {
             }
         }
         return list;
+    }
+
+    /**
+     * merge lists
+     * @param collections
+     * @param <T>
+     * @return
+     */
+    public static <T> Collection<T> merge(Collection<T>... collections) {
+        Collection<T> list = new ArrayList<T>();
+        for (Collection<T> node : collections) {
+            list.addAll(node);
+        }
+        return list;
+    }
+
+    /**
+     * merge arrays
+     * @param arrays
+     * @param <T>
+     * @return
+     */
+    public static <T> T[] merge(T[]... arrays) {
+        int sum = 0;
+        for (T[] arr : arrays) {
+            sum += arr.length;
+        }
+        T[] rv = (T[]) Array.newInstance(arrays.getClass().getComponentType().getComponentType(), sum);
+
+        int offset = 0;
+        for (T[] arr : arrays) {
+            System.arraycopy(arr, 0, rv, offset, arr.length);
+            offset += arr.length;
+        }
+
+        return rv;
     }
 }
