4fb9c815c44c068145d0818ce65a06dd6f62e74d
PARK Yong Seo <j@saro.me>
2024-07-06 21:14:09 +0900

    seq Pool 1

--- src/main/kotlin/me/saro/kit/service/SequencePool.kt
+++ src/main/kotlin/me/saro/kit/service/SequencePool.kt
@@ -1,30 +1,71 @@
 package me.saro.kit.service
 
-// test
-
-class SequencePool<T>(
-    private val minIssuedSequence: Int,
-    private val maxIssuedSequence: Int,
+class SequencePool<T> private constructor(
+    private val minPoolSize: Int,
+    private val maxPoolSize: Int,
+    private val increaseTimeMillis: Long,
+    private val decreaseTimeMillis: Long,
+    private val multiple: Int
 ) {
-    private val lock = Any()
-    private val store: ArrayDeque<T> = ArrayDeque(minIssuedSequence)
+    private val store: ArrayDeque<T> = ArrayDeque(minPoolSize)
+    private var lastIssuedSize: Int = minPoolSize
+    private var lastIssuedTime: Long = 0L
 
-    private var nowIssuedSize = minIssuedSequence
-    private var lastIssuedTime = 0L
+    @Synchronized
+    fun get(requestNewSequenceList: (needSequenceSize: Int) -> List<T>): T {
+        if (store.isEmpty()) {
+            store.addAll(requestNewSequenceList(needSequenceSize))
+        }
+        return store.removeFirst()
+    }
 
-    fun get(newIssue: (needSequenceSize: Int) -> List<T>): T {
-//        synchronized(lock) {
-//            if (store.isEmpty()) {
-//                val newSequences = newIssue(nowIssuedSize)
-//                if (newSequences.size != nowIssuedSize) {
-//                    throw IllegalStateException("newIssue return size is not equal to nowIssuedSize")
-//                }
-//                store.addAll(newSequences)
-//                lastIssuedTime = System.currentTimeMillis()
-//            }
-//            return store.removeFirst()
-//        }
-//        store.removeLastOrNull()
-        TODO()
+    fun resetLastIssuedTime() {
+        lastIssuedTime = 0L
+    }
+
+    val nowPoolSize: Int get() = lastIssuedSize
+    val nowStoreSize: Int get() = store.size
+
+    private val needSequenceSize: Int get() {
+        val now = System.currentTimeMillis()
+        if ((now - lastIssuedTime) < increaseTimeMillis) {
+            lastIssuedSize = (lastIssuedSize * multiple).coerceAtMost(maxPoolSize)
+        } else if ((now - lastIssuedTime) > decreaseTimeMillis) {
+            lastIssuedSize = (lastIssuedSize / multiple).coerceAtLeast(minPoolSize)
+        }
+        lastIssuedTime = now
+        return lastIssuedSize
+    }
+
+    class Builder() {
+        private var minPoolSize: Int = 1
+        private var maxPoolSize: Int = 100
+        private var increaseMinutes: Int = 1
+        private var decreaseMinutes: Int = 5
+        private var multiple: Int = 2
+
+        fun minPoolSize(minPoolSize: Int): Builder = this.apply { this.minPoolSize= minPoolSize
+        }
+        fun maxPoolSize(maxPoolSize: Int): Builder = this.apply { this.maxPoolSize = maxPoolSize }
+        fun increaseMinutes(increaseMinutes: Int): Builder = this.apply { this.increaseMinutes = increaseMinutes }
+        fun decreaseMinutes(decreaseMinutes: Int): Builder = this.apply { this.decreaseMinutes = decreaseMinutes }
+        fun multiple(multiple: Int): Builder = this.apply { this.multiple = multiple }
+
+        fun <T> build(): SequencePool<T> {
+            if (minPoolSize < 1 || maxPoolSize < minPoolSize) {
+                throw IllegalArgumentException("minPoolSize < 1 || maxPoolSize < minPoolSize")
+            }
+            if (increaseMinutes < 1 || decreaseMinutes < 1) {
+                throw IllegalArgumentException("increaseMinutes < 1 || decreaseMinutes < 1")
+            }
+            if (decreaseMinutes <= increaseMinutes) {
+                throw IllegalArgumentException("decreaseMinutes <= increaseMinutes")
+            }
+            if (multiple < 2) {
+                throw IllegalArgumentException("multiple < 2")
+            }
+
+            return SequencePool(minPoolSize, maxPoolSize, increaseMinutes * 60L, decreaseMinutes * 60L, multiple)
+        }
     }
 }
